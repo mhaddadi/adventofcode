83d1092b490a837379b16db61655e12fef477cf9
Manuel Haddadi <manuel.haddadi@gfi.fr>
2016-12-09 18:02:42 +0100

    Day 6 ok - Day 7 to debug

---
+++ src/main/java/adventofcode/Day6.java
@@ -0,0 +1,79 @@
+package adventofcode;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Day6 {
+	public static void main(String[] args) {
+		System.out.println("First star :");
+		firstStar(args);
+
+		System.out.println("Second star :");
+		secondStar(args);
+
+	}
+
+	private static void firstStar(String[] args) {
+		
+		Map<Integer, Map<String, Integer>> mapDeMap = new HashMap<>();
+		
+		for (String line : args) {
+
+			for (int i = 0; i < line.length(); i++) {
+				
+				Map<String, Integer> mapNbChar = mapDeMap.get(i);
+				
+				if (mapNbChar == null) {
+					mapNbChar = new HashMap<String, Integer>();
+				}
+				
+				String currentCar = String.valueOf(line.charAt(i));
+				
+				mapNbChar.put(currentCar, mapNbChar.get(currentCar) == null ? 1 : mapNbChar.get(currentCar) + 1);
+				
+				mapDeMap.put(i, mapNbChar);
+				
+			}
+			
+		}
+		
+		for (Map.Entry<Integer, Map<String, Integer>> entry : mapDeMap.entrySet()) {
+			
+			Map<String, Integer> mapTriee = sortByValue(entry.getValue());
+			
+			System.out.println(entry.getKey() + " : " + mapTriee.keySet());
+			
+		}
+		
+	}
+
+	private static void secondStar(String[] args) {}
+
+	private static Map<String, Integer> sortByValue(Map<String, Integer> map )
+	{
+		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
+		
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				int compare = o2.getValue().compareTo(o1.getValue());
+				if (compare == 0) {
+					compare = o1.getKey().compareTo(o2.getKey());
+				}
+				return compare;
+			}
+		});
+
+		Map<String, Integer>  result = new LinkedHashMap<String, Integer> ();
+		for (Map.Entry<String, Integer>  entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
+	}
+
+	
+}
---
+++ src/main/java/adventofcode/Day7.java
@@ -0,0 +1,93 @@
+package adventofcode;
+
+public class Day7 {
+	public static void main(String[] args) {
+		System.out.println("First star :");
+		firstStar(args);
+
+		System.out.println("Second star :");
+		secondStar(args);
+
+	}
+
+	private static void firstStar(String[] args) {
+		
+		int lineNb = 1;
+		int nbTlsIp = 0;
+		
+		for (String line : args) {
+			
+			System.out.println("line #" + lineNb + " : " + line);
+			
+			boolean isTlsIp = false;
+			
+			boolean isHypernetSeq = false;
+			
+			String[] firstSplit = line.split("\\[");
+			
+			for (String split : firstSplit) {
+				
+				String[] secondSplit = split.split("\\]");
+				
+				for (String subSplit : secondSplit) {
+					
+					if (isAbba(subSplit)) {
+						
+						if (isHypernetSeq) {
+							
+							System.out.println("isHypernetSeq => KO");
+							
+							isTlsIp = false;
+							break;
+							
+						} else {
+							
+							isTlsIp = true;
+							
+						}
+						
+					}
+
+					isHypernetSeq = !isHypernetSeq;
+					
+				}
+				
+			}
+			
+			if (isTlsIp) {
+				
+				nbTlsIp++;
+				System.out.println("OK TLS : nbTlsIp = " + nbTlsIp);
+			}
+			
+			lineNb++;
+			
+		}
+		
+		System.out.println(nbTlsIp);
+	}
+
+	private static void secondStar(String[] args) {}
+
+	private static boolean isAbba(String str) {
+		
+		if (str.length() < 4) {
+			return false;
+		}
+		
+		for (int i = 0; i < str.length() - 3; i++) {
+			
+			if (str.charAt(i) == str.charAt(i + 3) 
+					&& str.charAt(i + 1) == str.charAt(i + 2)
+					&& str.charAt(i + 2) != str.charAt(i + 3)) {
+				
+				System.out.println("ABBA found in " + str + " => " + str.substring(i, i + 4));
+				
+				return true;
+			}
+			
+		}
+		
+		return false;
+	}
+}
